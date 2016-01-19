7379b9ef096d72f90652b33a965e6d0da0cb29ff
KaiKikuchi <kai.kikuchi27@gmail.com>
2016-01-19 06:51:13 +0100

    Check proper WorldGuard flags.

--- src/main/java/net/kaikk/mc/fr/protectionplugins/WorldGuardHandler.java
+++ src/main/java/net/kaikk/mc/fr/protectionplugins/WorldGuardHandler.java
@@ -1,7 +1,5 @@
 package net.kaikk.mc.fr.protectionplugins;
 
-import net.kaikk.mc.fr.ProtectionHandler;
-
 import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Entity;
@@ -10,8 +8,12 @@ import org.bukkit.entity.Player;
 import com.sk89q.worldedit.BlockVector;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
 import com.sk89q.worldguard.protection.ApplicableRegionSet;
+import com.sk89q.worldguard.protection.flags.DefaultFlag;
+import com.sk89q.worldguard.protection.flags.StateFlag;
 import com.sk89q.worldguard.protection.regions.ProtectedCuboidRegion;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+
+import net.kaikk.mc.fr.ProtectionHandler;
 
 public class WorldGuardHandler implements ProtectionHandler {
 	WorldGuardPlugin worldGuard;
@@ -22,59 +24,80 @@ public class WorldGuardHandler implements ProtectionHandler {
 	
 	@Override
 	public boolean canBuild(Player player, Location location) {
-		return this.check(player, location);
+		return this.check(player, location, DefaultFlag.BUILD);
 	}
 
 	@Override
 	public boolean canAccess(Player player, Location location) {
-		return this.check(player, location);
+		return this.check(player, location, DefaultFlag.INTERACT);
 	}
 
 	@Override
 	public boolean canUse(Player player, Location location) {
-		return this.check(player, location);
+		return this.check(player, location, DefaultFlag.INTERACT);
 	}
 
 	@Override
 	public boolean canOpenContainer(Player player, Block block) {
-		return this.check(player, block.getLocation());
+		return this.check(player, block.getLocation(), DefaultFlag.CHEST_ACCESS);
 	}
 
 	@Override
 	public boolean canInteract(Player player, Location location) {
-		return this.check(player, location);
+		return this.check(player, location, DefaultFlag.INTERACT);
 	}
 
 	@Override
 	public boolean canAttack(Player player, Entity entity) {
-		return this.check(player, entity.getLocation());
+		return this.check(player, entity.getLocation(), DefaultFlag.PVP);
 	}
 
 	@Override
 	public boolean canProjectileHit(Player player, Location location) {
-		return this.check(player, location);
+		return this.check(player, location, DefaultFlag.PVP);
 	}
 	
 	@Override
 	public boolean canUseAoE(Player player, Location location, int range) {
-		ProtectedCuboidRegion pcr = new ProtectedCuboidRegion("ForgeRestrictorWGAoETest", new BlockVector(location.getBlockX()-range, 0, location.getBlockZ()-range), new BlockVector(location.getBlockX()+range, 255, location.getBlockZ()+range));
-		ApplicableRegionSet ars = this.worldGuard.getRegionManager(location.getWorld()).getApplicableRegions(pcr);
-		for (ProtectedRegion pr : ars.getRegions()) {
+		ApplicableRegionSet regions = this.getRegions(location, range);
+		boolean perm = regions.queryState(null, DefaultFlag.BUILD) == StateFlag.State.ALLOW && regions.queryState(null, DefaultFlag.PVP) == StateFlag.State.ALLOW;
+		if (!perm) {
+			this.permissionDeniedMessage(player);
+		}
+
+		return perm;
+		
+		/*for (ProtectedRegion pr : ars.getRegions()) {
 			if (!this.worldGuard.canBuild(player, new Location(location.getWorld(), pr.getMaximumPoint().getBlockX(), pr.getMaximumPoint().getBlockY(), pr.getMaximumPoint().getBlockZ()))) {
-				player.sendMessage("§4You don't have permission in this area.");
+				this.permissionDeniedMessage(player);
 				return false;
 			}
 		}
 		
-		return true;
+		return true;*/
 	}
 	
-	boolean check(Player player, Location location) {
-		boolean perm=this.worldGuard.canBuild(player, location);
+	protected boolean check(Player player, Location location, StateFlag flag) {
+		ApplicableRegionSet regions = this.getRegions(location);
+		boolean perm = regions.queryState(null, flag) == StateFlag.State.ALLOW;
 		if (!perm) {
-			player.sendMessage("§4You don't have permission in this area.");
+			this.permissionDeniedMessage(player);
 		}
+
 		return perm;
+	}
+	
+	protected ApplicableRegionSet getRegions(Location location) {
+		return this.worldGuard.getRegionManager(location.getWorld()).getApplicableRegions(location);
+	}
+	
+	protected ApplicableRegionSet getRegions(Location location, int range) {
+		ProtectedCuboidRegion pcr = new ProtectedCuboidRegion("ForgeRestrictorWGAoETest", new BlockVector(location.getBlockX()-range, 0, location.getBlockZ()-range), new BlockVector(location.getBlockX()+range, 255, location.getBlockZ()+range));
+		return this.worldGuard.getRegionManager(location.getWorld()).getApplicableRegions(pcr);
+	}
+	
+	protected void permissionDeniedMessage(Player player) {
+		player.sendMessage("§4You don't have permission in this area.");
 	}
 
 	@Override
